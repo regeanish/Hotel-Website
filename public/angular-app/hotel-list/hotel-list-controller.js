3b496cd80e568276d598f7033e7788104a14fe30
regeanish <regeanish@gmail.com>
2017-08-20 20:09:43 -0700

    added description for the list of hotels. Addtionally, user can post reviews and ratings along with their name

--- public/angular-app/hotel-list/hotel-list-controller.js
+++ public/angular-app/hotel-list/hotel-list-controller.js
@@ -1,11 +1,10 @@
-app.module('meanhotel').controller('HotelsController', HotelsController);
+angular.module('meanhotel').controller('HotelsController', HotelsController);
 
-function HotelsController($http){
-    var vm = this;
-    vm.title = 'Mean hotel app';
-
-    $http.get('/api/hotels?count=10').then(function(response){
+function HotelsController(hotelDataFactory) {
+  var vm = this;
+  vm.title = 'MEAN Hotel App';
+  hotelDataFactory.hotelList().then(function(response) {
+    // console.log(response);
     vm.hotels = response.data;
-    });
-
+  });
 }
